89531ac2f5f08f381754cc6536efa4074a26b3cc
ak <ak@192.168.1.8>
2021-07-29 23:38:15 +0530

    adding the changed files

---
+++ src/main/java/com/example/demo/StudentResources.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class StudentResources {
+	
+	@RequestMapping("students")
+	List<Students> getStudentDetails(){
+		
+		
+		List<Students> listStds = new ArrayList<Students>();
+			
+		Students student1=new Students();
+		student1.setName("ram");
+		student1.setStudentId(16);
+		student1.setDivision("division B");
+		student1.setAddress(" nanded city");
+		
+		Students student2=new Students();
+		student2.setName("raghav");
+		student2.setStudentId(16);
+		student2.setDivision("division A  ");
+		student2.setAddress("pune city");
+		
+		listStds.add(student1);
+		listStds.add(student2);
+		return listStds;
+		
+	}
+
+	
+
+}
---
+++ src/main/java/com/example/demo/Students.java
@@ -0,0 +1,46 @@
+
+package com.example.demo;
+
+public class Students {
+
+String name;
+
+String address;
+
+String  division;
+
+int studentId;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public String getDivision() {
+	return division;
+}
+
+public void setDivision(String division) {
+	this.division = division;
+}
+
+public int getStudentId() {
+	return studentId;
+}
+
+public void setStudentId(int studentId) {
+	this.studentId = studentId;
+}
+
+}
